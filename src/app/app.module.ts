acf6890c78d2f8ff4d4846d6f821e00cddf5d2f4
TonyZXY <xyzheng109@gmail.com>
2018-06-07 15:40:33 +1000

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { DataService } from './services/data.service';
 import { AppComponent } from './app.component';
 import { MenubarComponent } from './elements/menubar/menubar.component';
 import { ToolbarComponent } from './elements/toolbar/toolbar.component';
+import { SearchbarComponent } from './elements/searchbar/searchbar.component';
+import { NewseditComponent } from './webpages/news/newsedit/newsedit.component';
+import { NewsAddComponent } from './webpages/news/news-add/news-add.component';
+import { ListComponent } from './webpages/list/list.component';
+import { ListitemComponent } from './elements/listitem/listitem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenubarComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SearchbarComponent,
+    NewseditComponent,
+    NewsAddComponent,
+    ListComponent,
+    ListitemComponent
   ],
   imports: [
     BrowserModule
