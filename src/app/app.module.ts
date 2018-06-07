21ae78614ee000801528d5a5cf71ec0231c1d71d
zhangzeyao <460737263@qq.com>
2018-06-07 12:10:49 +1000

    Add structure to the web page file system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './services/data.service';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './elements/menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule
