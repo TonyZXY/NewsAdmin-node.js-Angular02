ca82e09295372a945744e96f565ebe2c59f21fc6
zhangzeyao <460737263@qq.com>
2018-08-02 15:09:05 +1000

    admin portal search delete

--- src/app/webpages/genuinesearch/genuinesearch.component.ts
+++ src/app/webpages/genuinesearch/genuinesearch.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Genuine} from '../../entities/Genuine';
+import {DataService} from '../../services/data.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-genuinesearch',
@@ -11,8 +13,12 @@ export class GenuinesearchComponent implements OnInit {
   public edit = '/genuine/edit/';
   public delete = '/genuine/delete/';
   @Input() genuines: Genuine[];
+  messageTitleToSend: string;
+  messageBodyToSend: string;
+  endDelete: boolean;
+  id: string;
 
-  constructor() { }
+  constructor(private dataService: DataService, private modalService: NgbModal) { }
 
   receiveMessage($event) {
     this.genuines = $event;
@@ -21,4 +27,27 @@ export class GenuinesearchComponent implements OnInit {
   ngOnInit() {
   }
 
+  onNotifySureToDelete(id: string, content) {
+    this.endDelete = false;
+    this.messageTitleToSend = '确认';
+    this.messageBodyToSend = '你确认删除这一条快讯吗 ?';
+    this.id = id;
+    this.modalService.open(content, {centered: true});
+  }
+  onDelete() {
+    this.dataService.deleteNewsFlash(this.id).subscribe(next => {
+      this.endDelete = true;
+      this.dataService.getGenuineList()
+        .subscribe(genuines => { this.genuines = genuines;
+        });
+    });
+    const err = this.dataService.errormessage;
+    if (err === '') {
+      this.messageTitleToSend = '成功删除';
+      this.messageBodyToSend = '已成功删除这一条快讯';
+    } else if (err !== '') {
+      this.messageTitleToSend = '错误';
+      this.messageBodyToSend = err;
+    }
+  }
 }
--- src/app/webpages/videosearch/videosearch.component.ts
+++ src/app/webpages/videosearch/videosearch.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Video} from '../../entities/Video';
+import {DataService} from '../../services/data.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-videosearch',
@@ -11,14 +13,42 @@ export class VideosearchComponent implements OnInit {
   public edit = '/video/edit/';
   public delete = '/news/delete/';
   @Input() videos: Video[];
+  messageTitleToSend: string;
+  messageBodyToSend: string;
+  endDelete: boolean;
+  id: string;
 
-  constructor() { }
+  constructor(private dataService: DataService, private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
   receiveMessage($event){
     this.videos = $event;
+  }
+
+  onNotifySureToDelete(id: string, content) {
+    this.endDelete = false;
+    this.messageTitleToSend = '确认';
+    this.messageBodyToSend = '你确认删除这一条快讯吗 ?';
+    this.id = id;
+    this.modalService.open(content, {centered: true});
+  }
+  onDelete() {
+    this.dataService.deleteNewsFlash(this.id).subscribe(next => {
+      this.endDelete = true;
+      this.dataService.getVideosList()
+        .subscribe(videos => { this.videos = videos;
+        });
+    });
+    const err = this.dataService.errormessage;
+    if (err === '') {
+      this.messageTitleToSend = '成功删除';
+      this.messageBodyToSend = '已成功删除这一条快讯';
+    } else if (err !== '') {
+      this.messageTitleToSend = '错误';
+      this.messageBodyToSend = err;
+    }
   }
 
 }
