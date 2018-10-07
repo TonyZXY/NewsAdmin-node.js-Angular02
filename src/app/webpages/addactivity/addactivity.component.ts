1c1a20ac3c406dac69ae59c5681a5b93f27540af
Tony Zheng <xyzheng109@gmail.com>
2018-10-08 10:31:44 +1100

    format code

--- src/app/webpages/addactivity/addactivity.component.ts
+++ src/app/webpages/addactivity/addactivity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Activity} from '../../entities/Activity';
 import {DataService} from '../../services/data.service';
 
@@ -18,21 +18,23 @@ export class AddactivityComponent implements OnInit {
   newDate: Date;
   time = {hour: 13, minute: 30};
   meridian = true;
-  constructor(private dataService: DataService) { }
+
+  constructor(private dataService: DataService) {
+  }
 
   ngOnInit() {
   }
 
   toggleMeridian() {
-      this.meridian = !this.meridian;
+    this.meridian = !this.meridian;
   }
 
   onSubmit() {
     this.newDate = new Date();
-    this.newDate.setFullYear(this.dateStartFormat.year,this.dateStartFormat.month-1,this.dateStartFormat.day)
-    this.newDate.setHours(this.time.hour,this.time.minute,0,0)
+    this.newDate.setFullYear(this.dateStartFormat.year, this.dateStartFormat.month - 1, this.dateStartFormat.day);
+    this.newDate.setHours(this.time.hour, this.time.minute, 0, 0);
     this.activityToAdd.eventStartTime = this.newDate.toISOString();
-    this.activityToAdd.eventID = (Math.random()).toString(36).substr(2, 9)
+    this.activityToAdd.eventID = (Math.random()).toString(36).substr(2, 9);
     this.activityToAdd.custom = true;
 
     const errorMessage = this.dataService.addActivity(this.activityToAdd);
