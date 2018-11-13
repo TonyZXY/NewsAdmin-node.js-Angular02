33c6460bd2efb74322c08d65d8d980e4065bac19
Tony Zheng <xyzheng109@gmail.com>
2018-11-14 10:36:56 +1100

    changes

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -22,8 +22,8 @@ const httpOptions = {
 })
 
 export class DataService {
-  // private urlHead = 'https://cryptogeekapp.com/api';
-  private urlHead = 'http://192.168.9.92:3020/api';
+  private urlHead = 'https://cryptogeekapp.com/api';
+  // private urlHead = 'http://192.168.9.92:3020/api';
   errormessage = '';
 
 
